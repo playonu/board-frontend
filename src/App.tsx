4a7f6492c48f41fd53e8f7481afcdd7067fc2c5e
boom0704 <relonging@naver.com>
2023-07-04 15:55:19 +0900

    feat : Home 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,36 @@
-import React from 'react';
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import { PATH } from "./utils/constants";
 
 function App() {
+  const navigate = useNavigate();
+
+  const handleMovePage = (page: string) => {
+    navigate(`${page}`);
+  };
+
   return (
-    <div className="App">
-      홈
-      <div className="bg-blue-500"> Tailwind Css 적용 테스트 </div>
+    <div className="flex justify-center items-center h-screen">
+      <div>
+        <button
+          className="mx-4 h-[120px] w-[120px] bg-black text-white hover:bg-zinc-700 sm:h-[160px] sm:w-[160px] rounded-[20px]"
+          onClick={() => handleMovePage(PATH.signIn)}
+        >
+          signIn
+        </button>
+        <button
+          className="mx-4 h-[120px] w-[120px] bg-black text-white hover:bg-zinc-700 sm:h-[160px] sm:w-[160px] rounded-[20px]"
+          onClick={() => handleMovePage(PATH.signUp)}
+        >
+          signUp
+        </button>
+        <button
+          className="mx-4 h-[120px] w-[120px] bg-black text-white hover:bg-zinc-700 sm:h-[160px] sm:w-[160px] rounded-[20px]"
+          onClick={() => handleMovePage(PATH.board)}
+        >
+          Board
+        </button>
+      </div>
     </div>
   );
 }
